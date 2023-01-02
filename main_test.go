2c1c48243ea1e0627c783c75c0b7607404031422
sirzzang <sirzzang@naver.com>
2023-01-02 20:44:40 +0900

    feat: update table tests

--- main_test.go
+++ main_test.go
@@ -4,21 +4,32 @@ import "testing"
 
 func Test_isPrime(t *testing.T) {
 
-	result, msg := isPrime(0)
-	if result {
-		t.Errorf("with %d as test parameter, got true, but expected false", 0)
-	}
-	if msg != "0 is not prime, by definition!" {
-		t.Error("wrong message returned:", msg)
+	primeTests := []struct {
+		name     string
+		testNum  int
+		expected bool
+		msg      string
+	}{
+		{"prime", 7, true, "7 is a prime number!"},
+		{"no prime", 1, false, "1 is not prime, by definition!"},
+		{"prime", 8, false, "8 is not a prime number, because it is divisible by 2!"},
+		{"prime", -3, false, "Negative numbers are not prime, by definition!"},
 	}
 
-	result, msg = isPrime(7)
-	if !result {
-		t.Errorf("with %d as test parameter, got false, but expected true", 7)
-	}
-	if msg != "7 is a prime number!" {
-		t.Error("wrong message returned:", msg)
+	for _, e := range primeTests {
 
+		result, msg := isPrime(e.testNum)
+		if e.expected && !result {
+			t.Errorf("%s: expected true, but got false", e.name)
+		}
+
+		if !e.expected && result {
+			t.Errorf("%s: expected false, but got true", e.name)
+		}
+
+		if e.msg != msg {
+			t.Errorf("%s expected %s, but got %s", e.name, e.msg, msg)
+		}
 	}
 
 }
